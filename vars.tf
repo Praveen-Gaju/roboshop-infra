13801add8754357f8fd8506610d91b31bab3bc4f
Praveen Gajulapalle <Praveen.gaju8994@gmail.com>
2023-04-25 17:48:27 +0530

    updating roboshop-infra directory

--- vars.tf
+++ vars.tf
@@ -1,29 +1,36 @@
 variable "instances" {
   default = {
     frontend = {
-      name = "frontend"
-      type = "t3.micro"
+      name      = "frontend"
+      type      = "t3.micro"
+      password  = "123"
     }
     mongodb = {
-      name = "mongodb"
-      type = "t3.micro"
+      name      = "mongodb"
+      type      = "t3.micro"
+      password  = "123"
     }
     catalogue = {
-      name = "catalogue"
-      type = "t3.micro"
+      name      = "catalogue"
+      type      = "t3.micro"
+      password  = "123"
     }
     redis = {
-      name = "redis"
-      type = "t3.micro"
+      name      = "redis"
+      type      = "t3.micro"
+      password  = "123"
     }
 
     user = {
-      name = "user"
-      type = "t3.micro"
+      name      = "user"
+      type      = "t3.micro"
+      password  = "123"
     }
     cart = {
-      name = "cart"
-      type = "t3.micro"
+      name      = "cart"
+      type      = "t3.micro"
+      password  = "123"
+
     }
     mysql = {
       name      = "mysql"
