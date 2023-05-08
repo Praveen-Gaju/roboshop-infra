fb865ebcd8a4cf58866ac74dbdb4418a42750f16
Praveen Gajulapalle <Praveen.gaju8994@gmail.com>
2023-05-08 19:53:44 +0530

    updating roboshop-infra directory

--- aws-parameters/main.tf
+++ aws-parameters/main.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "parameter" {
-  count = (var.parameters)
+  count = length(var.parameters)
   name  = var.parameters[count.index].name
   type  = var.parameters[count.index].type
   value = var.parameters[count.index].value
