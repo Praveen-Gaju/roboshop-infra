e194b648da2e57302244684bedc34bed33053e78
Praveen Gajulapalle <Praveen.gaju8994@gmail.com>
2023-05-26 23:00:19 +0530

    updating roboshop-infra directory

--- main.tf
+++ main.tf
@@ -2,8 +2,11 @@ module "vpc" {
   source          = "git::https://github.com/Praveen-Gaju/tf-module-vpc.git"
   env             = var.env
   tags            = var.tags
+  default_route_table = var.default_route_table
+  default_vpc_id = var.default_vpc_id
   for_each        = var.vpc
   vpc_cidr        = each.value["vpc_cidr"]
   public_subnets  = each.value["public_subnets"]
   private_subnets = each.value["private_subnets"]
+
 }
