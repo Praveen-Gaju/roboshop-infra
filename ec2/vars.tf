553c0298d08d9b109b5e11f529aea8fbea89d3b5
Praveen Gajulapalle <Praveen.gaju8994@gmail.com>
2023-05-26 18:23:01 +0530

    updating roboshop-infra directory

--- ec2/vars.tf
+++
@@ -1,6 +0,0 @@
-#variables
-variable "component" {}
-variable "instance_type" {}
-variable "env" {}
-variable "monitor" {}
-#variable "password" {}   ##used while executing script using shell script
--- main.tf
+++ main.tf
@@ -1,9 +1,4 @@
-module "ec2" {
-  for_each      = var.instances
-  source        = "./ec2"
-  component     = each.value["name"]
-  instance_type = each.value["type"]
-  env           = var.env
-  monitor       = try(each.value["monitor"], false)
-  #password      = try(each.value["password"], "null") ##used when executing script using shell
+module "vpc" {
+  source = "https://github.com/Praveen-Gaju/tf-module-vpc.git"
+  for_each = var.vpc
 }
