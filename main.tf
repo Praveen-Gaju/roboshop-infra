149c3403a8498f8fe9426887e6a2e3378f077d37
Praveen Gajulapalle <Praveen.gaju8994@gmail.com>
2023-05-26 18:47:10 +0530

    updating roboshop-infra directory

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 module "vpc" {
   source      = "https://github.com/Praveen-Gaju/tf-module-vpc.git"
   env         = var.env
+  tags        = var.tags
   for_each    = var.vpc
   vpc_cidr    = each.value["vpc_cidr"]
 }
