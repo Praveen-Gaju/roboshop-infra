1d3148adb5447c90eed6ecb444c45de67cd292e5
Praveen Gajulapalle <Praveen.gaju8994@gmail.com>
2023-05-31 16:51:53 +0530

    updating roboshop-infra directory

--- main.tf
+++ main.tf
@@ -89,4 +89,5 @@ module "app" {
   desired_capacity        = each.value["desired_capacity"]
   max_size                = each.value["max_size"]
   min_size                = each.value["min_size"]
+  subnets                 = lookup(local.subnet_ids, each.value["subnet_name"], null )
 }
