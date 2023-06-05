7e1cce4f140a756f66c9615df9d347f9b8437706
Praveen Gajulapalle <Praveen.gaju8994@gmail.com>
2023-06-05 13:11:16 +0530

    updating roboshop-infra directory

--- main.tf
+++ main.tf
@@ -101,3 +101,7 @@ module "app" {
   allow_app_to            = lookup(local.subnet_cidr, each.value["allow_app_to"], null)
   alb_dns_name            = lookup(lookup(lookup(module.alb, each.value["alb"], null), "alb", null), "dns_name", null)
 }
+
+output "alb" {
+  value = module.alb
+}
