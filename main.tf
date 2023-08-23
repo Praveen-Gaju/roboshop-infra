7f459d14216a75c3e38b05d671538f42d7463074
Praveen Gajulapalle <Praveen.gaju8994@gmail.com>
2023-08-23 12:44:59 +0530

    updating roboshop-infra directory

--- main.tf
+++ main.tf
@@ -168,6 +168,7 @@ resource "null_resource" "load-gen" {
 */
 
 #Minikube Module
+/*
 module "minikube" {
   source              = "github.com/scholzj/terraform-aws-minikube"
 
@@ -198,6 +199,7 @@ output "MINIKUBE_SERVER" {
 output "KUBE_CONFIG" {
   value = "scp centos@${module.minikube.public_ip}:/home/centos/kubeconfig ~/.kube/config"
 }
+*/
 
 #EKS Module
 
