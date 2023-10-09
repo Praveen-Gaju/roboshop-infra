61cce30094cec5448553a7866b9c1e0cf5894f25
Praveen Gajulapalle <Praveen.gaju8994@gmail.com>
2023-10-09 17:51:09 +0530

    updating roboshop-infra directory

--- data.tf
+++ data.tf
@@ -8,6 +8,14 @@ data "aws_ami" "ami" {
 */
 
 
+#centos-8 image
+data "aws_ami" "ami" {
+  most_recent   = true
+  name_regex    = "Centos-8-DevOps-Practice"
+  owners        = ["973714476881"]
+}
+
+
 data "aws_ssm_parameter" "ssh_pass" {
   name = "${var.env}.ssh.password"
 }
